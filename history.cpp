4b4061a9ffb5fec1495bf079d3a76061b133f0a5
liang123481 <177709007+liang123481@users.noreply.github.com>
2024-12-14 20:26:28 +0800

    Create history.cpp

---
+++ history.cpp
@@ -0,0 +1,8 @@
+#pragma once
+#include"history.h"
+history_ATM::history_ATM(int money, int type, int toid, int firstid) {
+	this->money = money;
+	this->type = type;
+	this->toID = toid;
+	this->firstID = firstid;
+}
