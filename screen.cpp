4a46d4e27c6e83b60e8fd225f2251c22b3351084
liang123481 <177709007+liang123481@users.noreply.github.com>
2024-12-15 15:31:04 +0800

    Update screen.cpp

--- screen.cpp
+++ screen.cpp
@@ -1,20 +1,20 @@
 #pragma once
 #include"screen.h"
 void screen::printmenu() {
-	cout << "       1.inputmoney       " << endl << "       2.takemoney       " << endl << "       3.tranmoney       " << endl << "       4.check account information       "<<endl<<"       5.exit      "<<endl;
+	cout << "                                                 1.Withdraw" << endl <<endl<< "                                                 2.Deposit       " << endl <<endl<< "                                                 3.Transfer       " << endl <<endl<< "                                                 4.check account information       "<<endl<<endl<<"                                                 5.exit      "<<endl<<endl;
 }
 void screen::print(string s) {
 	cout << s << endl;
 }
 void screen::print(string name, int id, int money) {
-	cout << "Name:" << name << endl << "ID:" << id << endl << "cash:" << money << endl;
+	cout << "                                                 Name:" << name << endl << "                                                 ID:" << id << endl << "                                                 cash:" << money << endl;
 }
 void screen::print(int a,int b,int c,int d) {
 	if (d == 0) {
-		cout << "type:" << b << endl << "operid:" << c << endl << "money:" << a << endl;
+		cout << "                                                 type:" << b << endl << "                                                 operid:" << c << endl << "                                                 money:" << a << endl;
 	}
 	else {
-		cout << "type:" << b << endl<<c << "operid tran to id:" << d << endl << "money:" << a << endl;
+		cout << "                                                 type:" << b << endl<<c << "                                                 operid tran to id:" << d << endl << "                                                 money:" << a << endl;
 	}
 }
 void screen::screen_clear() {
@@ -25,5 +25,5 @@ void screen::clear_afterexit() {
 	system("cls");
 }
 void screen::print(int cash) {
-	cout << "cash:" << cash << endl;
+	cout << "                                                 cash:" << cash << endl;
 }
