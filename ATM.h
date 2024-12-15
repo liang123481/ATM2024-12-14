f28a9a435489b54d6ca0f90e6cbcb0c0caa505ed
liang123481 <177709007+liang123481@users.noreply.github.com>
2024-12-15 15:39:39 +0800

    Update ATM.h

--- ATM.h
+++ ATM.h
@@ -6,14 +6,13 @@ using namespace std;
 #include"account.h"
 #include"keypad.h"
 #include"screen.h"
+#include"trade.h"
 class ATM {
 public:
 	int cash;
-	accountdatabase a;
 	ATMlist historylist;
 	screen screen;
 	keypad keypad;
 	ATM();
-	void start();
+	void start(Database_port& a);
 };
-bool checkisenough(int cash, int want);
